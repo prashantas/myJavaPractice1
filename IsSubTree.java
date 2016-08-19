de790a4ca6f2508f25787c72e5cc3f686fb77829
Prashanta Sharma <prashanta.tezu@gmail.com>
2016-08-19 17:24:58 +0530

    Create IsSubTree.java

---
+++ IsSubTree.java
@@ -0,0 +1,2 @@
+// http://algorithms.tutorialhorizon.com/given-two-binary-trees-check-if-one-binary-tree-is-a-subtree-of-another/
+// http://www.geeksforgeeks.org/check-binary-tree-subtree-another-binary-tree-set-2/
