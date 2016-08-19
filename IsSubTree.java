86cc413c3129358d6ee8f5dceef12c929e8fae17
Prashanta Sharma <prashanta.tezu@gmail.com>
2016-08-19 20:54:15 +0530

    Update IsSubTree.java

--- IsSubTree.java
+++ IsSubTree.java
@@ -1,2 +1,3 @@
 // http://algorithms.tutorialhorizon.com/given-two-binary-trees-check-if-one-binary-tree-is-a-subtree-of-another/
+// http://www.geeksforgeeks.org/check-if-a-binary-tree-is-subtree-of-another-binary-tree/
 // http://www.geeksforgeeks.org/check-binary-tree-subtree-another-binary-tree-set-2/
