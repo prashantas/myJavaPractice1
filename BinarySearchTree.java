764770716978187ff3f0dd046f11884677a0f3f1
Prashanta Sharma <prashanta.tezu@gmail.com>
2016-09-29 21:16:48 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -273,6 +273,37 @@ class BST {
     		return equals(n1, n2) || isSubtree(n1.left, n2) || isSubtree(n1.right, n2);
 	}
 	
+	//***************************************************************************************************
+	/*
+     Given a tree and a sum, return true if there is a path from the root
+     down to a leaf, such that adding up all the values along the path
+     equals the given sum.
+   
+     Strategy: subtract the node value from the sum when recurring down,
+     and check to see if the sum is 0 when you run out of tree.
+     */
+  
+    boolean haspathSum(Node node, int sum) 
+    {
+        if (node == null)
+            return (sum == 0);
+        else
+        {
+            boolean ans = false;
+  
+            /* otherwise check both subtrees */
+            int subsum = sum - node.data;
+            if (subsum == 0 && node.left == null && node.right == null)
+                return true;
+            if (node.left != null)
+                ans = ans || haspathSum(node.left, subsum);
+            if (node.right != null)
+                ans = ans || haspathSum(node.right, subsum);
+            return ans;
+        }
+    }
+     
+	
 }
 
 public class BinarySearchTree {
